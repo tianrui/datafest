c4fd91b95bd9cc189e919ebe881e3041d1d9c4d2
Tianrui Xiao <rxiao@cs.toronto.edu>
2016-04-30 11:49:46 -0400

    Preprocessing purchases, need to debug

--- Data and Codebook/utils.py
+++ Data and Codebook/utils.py
@@ -1,28 +1,33 @@
 import csv
 import numpy as np
 import tensorflow as tf
+import pdb
 
 """
 Load data
 """
-def load_adwords():
+def load_adwords(endind):
     fname = "approved_adwords_v3.csv"
-    return load(fname)
+    return load(fname, endind)
 
-def load_purchases():
+def load_purchases(endind):
     fname = "approved_data_purchase-v5.csv"
-    return load(fname)
+    return load(fname, endind)
 
-def load_ga():
+def load_ga(frac):
     fname = "approved_ga_data_v2.csv"
-    return load(fname)
+    return load(fname, endind)
 
-def load(fname):
+def load(fname, endind):
     reader = csv.DictReader(open(fname))
     res = {}
+    i = 0
     for row in reader:
         for col, val in row.iteritems():
             res.setdefault(col, []).append(val)
+        i += 1
+        #if i < endind:
+        #    break
 
     return res
 
@@ -39,6 +44,45 @@ def dict2array(datadict):
         data[i] = np.asarray(datadict[key], dtype=float32)
 
     return data
+
+def str2num(strlist):
+    """Convert string list to one hot encoded array
+
+    Input: strlist list
+    Output: data array
+    """
+    keys, ind = np.unique(strlist, return_inverse=True)
+    data = np.zeros((len(strlist), len(keys)), dtype=int)
+        
+    data[np.arange(len(strlist)), ind] = 1
+        
+    return data
+
+def preproc_purchases(frac=1):
+    """ Preprocess the purchase csv, convert to 2D array
+    with numerical values, convert categorical data to one hot
+    encoding.
+
+    Input: None
+    Output: result array
+    """
+    rawdict = load_purchases(100)
+    size = len(raw_dict[raw_dict.keys()[0]])
+    endind = int(size*frac)
+    converted_list = []
+    converted_list.append(str2num(rawdict['event_id'][:endind]))
+    converted_list.append(str2num(rawdict['primary_act_id'][:endind]))
+    converted_list.append(str2num(rawdict['major_cat_name'][:endind]))
+    converted_list.append(str2num(rawdict['minor_cat_name'][:endind]))
+    converted_list.append(str2num(rawdict['venue_city'][:endind]))
+    converted_list.append(str2num(rawdict['venue_state'][:endind]))
+    converted_list.append(rawdict['venue_postal_cd_sgmt_1'][:endind])
+    converted_list.append(str2num(rawdict['la_event_type_cat'][:endind]))
+    converted_list.append(str2num(rawdict['delivery_type_cd'][:endind]))
+
+    result = np.concatenate(converted_list, axis=1)
+    return result
+
 
 def reduce_logsumexp(input_tensor, reduction_indices=1, keep_dims=False):
   """Computes the sum of elements across dimensions of a tensor in log domain.
